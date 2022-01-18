dead27ce665bd5f650dd26362f9806065216a455
invakid404 <invakid404@riseup.net>
2022-01-18 11:15:35 +0200

    feat: initialize workspace

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,51 @@
 import * as core from '@actions/core';
+import { DendronConfig as DendronWorkspaceConfig } from '@dendronhq/common-all';
+import { DendronConfig as DendronGlobalConfig } from '@dendronhq/common-all/lib/types/configs/dendronConfig';
+import * as childProcess from 'child_process';
+import * as fs from 'fs';
+import * as yaml from 'js-yaml';
+import * as shlex from 'shlex';
+
+type DendronConfig = DendronWorkspaceConfig & DendronGlobalConfig;
+
+const configName = 'dendron.yml';
+
+const doIgnorePrivate = (config: DendronConfig): void => {
+  config.workspace.vaults = config.workspace.vaults.filter(
+    ({ visibility }) => visibility !== 'private',
+  );
+
+  if (
+    config.site.duplicateNoteBehavior &&
+    Array.isArray(config.site.duplicateNoteBehavior.payload)
+  ) {
+    const nameSet = new Set(config.workspace.vaults.map(({ name }) => name));
+
+    config.site.duplicateNoteBehavior.payload =
+      config.site.duplicateNoteBehavior.payload.filter((name) =>
+        nameSet.has(name),
+      );
+  }
+
+  fs.writeFileSync(configName, yaml.dump(config));
+};
 
 (async (): Promise<void> => {
+  const dendronCommand = shlex.split(core.getInput('dendron-cli-command'));
   const ignorePrivate = core.getBooleanInput('ignore-private');
-  core.info(`Ignore private: ${ignorePrivate}`);
+
+  const dendronConfigData = fs.readFileSync(configName, 'utf8');
+  const dendronConfig = yaml.load(dendronConfigData) as DendronConfig;
+
+  if (ignorePrivate) {
+    doIgnorePrivate(dendronConfig);
+  }
+
+  core.info('Initializing workspace');
+
+  const initCommand = [...dendronCommand, 'workspace', 'init']
+    .map((arg) => shlex.quote(arg))
+    .join(' ');
+
+  childProcess.execSync(initCommand);
 })();
